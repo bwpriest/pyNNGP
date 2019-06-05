7daf317b5404b376279010c354c6bb2d4c5d3c86
Ben Priest <priest2@llnl.gov>
2019-06-05 10:49:27 -0700

    Added flags to python API for LinearNNGP and SeqNNGP that allow row-stochastic normalization of coordinates. Also added starting docstrings to python API.

--- pyNNGP/LinearNNGP.py
+++ pyNNGP/LinearNNGP.py
@@ -3,8 +3,47 @@ import numpy as np
 
 
 class LinearNNGP:
+    """Linear Spatial Hierarchical Nearest Neighgor Gaussian Process (LinearNNGP)
+
+    The implementation is based upon Datta, Banerjee, Finley, and Gelfand,
+    "Hierarchical Nearest-Neighbor Guassian Process Models for Large
+    Geostatistical Datasets" (https://arxiv.org/abs/1406.7343).
+
+    We make the following assumtions:
+        - the fixed design matrix X from Eq. (8) of [DBFG16] is the identity
+          matrix.
+
+    Parameters
+    ----------
+    y : array-like
+        The target values observed at the training samples.
+    X : array-like, shape = (n_samples, n_features)
+        The fixed spatially-reference predictors of the linear model.
+    coords : array-like, shape = (n_samples, n_features)
+        The coordinate tuples corresponding to the observed targets.
+    nNeighbors : int
+        The maximum number of nearest neighbors upon which sample likelihood
+        functions may be conditioned.
+    covModel : CovModel object
+        The kernel specifying the covariance function of the GP.
+    distFunc : DistFunc object
+        The function determining how nearest neighbors will be assigned.
+    noiseModel : NoiseModel object
+        The model specifying priors on the model noise.
+    normalize : boolean, optional (default: False)
+        Flag indicating whether to transform coords to be row-stochastic.
+    """
+
     def __init__(
-        self, y, X, coords, nNeighbors, covModel, distFunc, noiseModel
+        self,
+        y,
+        X,
+        coords,
+        nNeighbors,
+        covModel,
+        distFunc,
+        noiseModel,
+        normalize=False,
     ):
         self.X = np.ascontiguousarray(np.atleast_2d(X))
         self.y = np.ascontiguousarray(np.atleast_1d(y))
@@ -15,21 +54,31 @@ class LinearNNGP:
         self.distFunc = distFunc
         self.noiseModel = noiseModel
 
+        if noramlize == True:
+            self.coords = self.coords / self.coords.sum(axis=0)
+
         assert self.coords.shape[0] == self.X.shape[0] == self.y.shape[0]
         self._LinearNNGP = _pyNNGP.LinearNNGP(
-            self.y.ctypes.data,
-            self.X.ctypes.data,
-            self.coords.ctypes.data,
-            self.coords.shape[1],
-            self.X.shape[1],
-            self.coords.shape[0],
-            self.nNeighbors,
-            self.covModel,
-            self.distFunc,
-            self.noiseModel,
+            self.y.ctypes.data,  # target values
+            self.X.ctypes.data,  # fixed spatially-referenced predictors
+            self.coords.ctypes.data,  # input locations
+            self.coords.shape[1],  # # of input dimensions
+            self.X.shape[1],  # # of indicators per input location
+            self.coords.shape[0],  #  # of location/target pairs
+            self.nNeighbors,  # maximum # of nearest neighbors for conditioning
+            self.covModel,  # covariance function to be used
+            self.distFunc,  # distance/similarity function to be used
+            self.noiseModel,  # noise prior
         )
 
     def sample(self, N):
+        """Sample N functions from LinearNNGP and evaluate at training points.
+
+        Parameters
+        ----------
+        N : int
+            Number of samples to return
+        """
         self._LinearNNGP.sample(N)
 
     def updateW(self):
--- pyNNGP/SeqNNGP.py
+++ pyNNGP/SeqNNGP.py
@@ -3,7 +3,41 @@ import numpy as np
 
 
 class SeqNNGP:
-    def __init__(self, y, coords, nNeighbors, covModel, distFunc, noiseModel):
+    """Nearest Neighgor Gaussian Process (NNGP)
+
+    The implementation is based upon Datta, Banerjee, Finley, and Gelfand,
+    "Hierarchical Nearest-Neighbor Guassian Process Models for Large
+    Geostatistical Datasets" (https://arxiv.org/abs/1406.7343).
+
+    Parameters
+    ----------
+    y : array-like
+        The target values observed at the training samples.
+    coords : array-like, shape = (n_samples, n_features)
+        The coordinate tuples corresponding to the observed targets.
+    nNeighbors : int
+        The maximum number of nearest neighbors upon which sample likelihood
+        functions may be conditioned.
+    covModel : CovModel object
+        The kernel specifying the covariance function of the GP.
+    distFunc : DistFunc object
+        The function determining how nearest neighbors will be assigned.
+    noiseModel : NoiseModel object
+        The model specifying priors on the model noise.
+    normalize : boolean, optional (default: False)
+        Flag indicating whether to transform coords to be row-stochastic.
+    """
+
+    def __init__(
+        self,
+        y,
+        coords,
+        nNeighbors,
+        covModel,
+        distFunc,
+        noiseModel,
+        normalize=False,
+    ):
         self.y = np.ascontiguousarray(np.atleast_1d(y))
         # Sort by coords[:, 0] first?
         self.coords = np.ascontiguousarray(np.atleast_2d(coords))
@@ -12,18 +46,29 @@ class SeqNNGP:
         self.distFunc = distFunc
         self.noiseModel = noiseModel
 
+        if noramlize == True:
+            self.coords = self.coords / self.coords.sum(axis=0)
+
+        assert self.coords.shape[0] == self.y.shape[0]
         self._SeqNNGP = _pyNNGP.SeqNNGP(
             self.y.ctypes.data,  # target values
-            self.coords.ctypes.data,  # input locations
-            self.coords.shape[1],  #
-            self.coords.shape[0],
-            self.nNeighbors,
-            self.covModel,
-            self.distFunc,
-            self.noiseModel,
+            self.coords.ctypes.data,  # input features
+            self.coords.shape[1],  # # of input of dimensions
+            self.coords.shape[0],  # # of sample/target pairs
+            self.nNeighbors,  # maximum # of nearest neighbors for conditioning
+            self.covModel,  # covariance function to be used
+            self.distFunc,  # distance/similarity function to be used
+            self.noiseModel,  # noise prior
         )
 
     def sample(self, N):
+        """Sample N functions from NNGP and evaluate at training points.
+
+        Parameters
+        ----------
+        N : int
+            Number of samples to return
+        """
         self._SeqNNGP.sample(N)
 
     def updateW(self):
